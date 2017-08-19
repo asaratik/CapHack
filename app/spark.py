e3123fe914497fe6237a111c335e2ac1040a1a17
ashok942 <ashokaratikatla@gmail.com>
2017-08-19 16:24:46 -0700

    Added spark and db support

---
+++ app/spark.py
@@ -0,0 +1,8 @@
+from ciscosparkapi import CiscoSparkAPI
+import config 
+
+api = CiscoSparkAPI(config.API_KEY)
+
+def list_rooms(keyword):
+	all_rooms = api.rooms.list() 
+	demo_rooms = [room for room in all_rooms if keyword in room.title]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import request, render_template
+import db
+import spark
 import sys
 
 @app.route('/')
 def index():
     return  "hello"
+
+@app.route('/list_rooms')
+def list_rooms():
+	return spark.list_rooms('CAPHACK')
