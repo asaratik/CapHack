4e5a9d874612213d407f3b99614ad6eb0f1846af
ashok942 <ashokaratikatla@gmail.com>
2017-08-19 16:39:52 -0700

    updated list call

--- app/spark.py
+++ app/spark.py
@@ -6,3 +6,4 @@ api = CiscoSparkAPI(config.API_KEY)
 def list_rooms(keyword):
 	all_rooms = api.rooms.list() 
 	demo_rooms = [room for room in all_rooms if keyword in room.title]
+	return demo_rooms
--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import sys
 def index():
     return  "hello"
 
-@app.route('/list_rooms')
-def list_rooms():
-	return spark.list_rooms('CAPHACK')
+@app.route('/list_rooms/<keyword>')
+def list_rooms(keyword):
+	roomdata = ', '.join([str(room) for room in spark.list_rooms(keyword)])
+	return "<h1>"+roomdata+"</h1>"
