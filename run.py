657719787d705568276ebbd0e26d0d9768b55088
ashok942 <ashokaratikatla@gmail.com>
2017-08-19 20:17:00 -0700

    Redesign

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 #!flask/bin/python
-from app import app
-app.run(debug=True)
+from flask import request, render_template
+import db
+import spark
+import sys
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return  "hello"
+
+@app.route('/list_rooms/<keyword>')
+def list_rooms(keyword):
+	roomdata = ', '.join([str(room) for room in spark.list_rooms(keyword)])
+	return "<h1>"+roomdata+"</h1>"
+
+@app.route('/web_hook', methods=['POST'])
+def web_hook():
+	body = json.loads(request.data)
+	return body
+
+if __name__ == '__main__':
+	app.run(debug=True)
