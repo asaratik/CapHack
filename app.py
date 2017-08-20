64efc95c277154299de664470fd57874b02512ee
Abhi <abhiram.304@gmail.com>
2017-08-20 12:03:14 -0700

    Test1

--- app.py
+++ app.py
@@ -94,9 +94,8 @@ def addParticipantsToRoom(roomId, email_addresses):
             'postman-token': "7658b5dc-3a90-c4cf-9782-781a05d16eb1"
             }
 
-response = requests.request("POST", url, data=payload, headers=headers)
-
-print(response.text)
+           response = requests.request("POST", url, data=payload, headers=headers)
+           print(response.text)
 
         print("77777777777777777",r)
     except requests.HTTPError as e:
