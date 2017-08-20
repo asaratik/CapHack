2b5faa7ab28f9d10a1849aa184a7eb841aa08637
Abhi <abhiram.304@gmail.com>
2017-08-19 22:12:58 -0700

    test webhook for API.AI

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 def index():
     return  "hello"
 
+@app.route('/worker', methods=['POST'])
+def worker_serve():
+    res =    {
+    "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+    "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+    "data":{},
+    "contextOut": [],
+    "source": "DuckDuckGo"
+    }
+    return json.dumps(res), 201
+
 @app.route('/list_rooms/<keyword>')
 def list_rooms(keyword):
 	roomdata = ', '.join([str(room) for room in spark.list_rooms(keyword)])
@@ -20,4 +31,5 @@ def web_hook():
 
 if __name__ == '__main__':
 	app.secret_key = os.urandom(12)
-	#app.run(debug=True)
+	app.run(debug=True)
+    
